d7562f3b652027a1b27575d661da0f7d4e38a972
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-03-22 15:47:36 +0100

    Auth, login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { LoginDetails } from './interfaces/login-details';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'frontEnd';
+
+  loginDetails: LoginDetails; 
+
+  constructor(auth: AuthService){
+    this.loginDetails = {
+      email:'user@user.user',
+      password:'user'
+    }
+
+    auth.loginUser(this.loginDetails);
+    
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoginDetails } from '../interfaces/login-details';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseUrl = 'http://127.0.0.1/api/';
+
+  private httpOptions ={
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+  constructor(private http:HttpClient) {  }
+
+  loginUser(loginDetails: LoginDetails){
+    this.http.post<any>(this.baseUrl+'login', loginDetails, this.httpOptions).pipe(
+      catchError(this.handleError)).subscribe(result =>{
+        console.log(result);
+        localStorage.setItem("token", result.token);
+      })
+  }
+  private handleError(error:HttpErrorResponse){
+    if(error.status === 404){
+      console.error('an error occurred:', error.error);
+    }else {
+      console.error(
+        `backend returned error code: ${error.status}, body was`, error.error); 
+      
+    }
+    return throwError(()=> new Error('somthing went wrong, please try again later.'));
+  }
 }
