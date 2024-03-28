25bc173c0f8831a19ce206a5aa727e99ef8fe797
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-03-28 16:48:37 +0100

    error fix

--- src/app/interfaces/recipe.ts
+++
@@ -1,8 +0,0 @@
-export interface Recipe {
-    //här fyller vi i vilka delar vi vill ta ut från vårt api recipie
-    label: string,
-    image: string, 
-    ingredientLines: string,
-    totalTime: number,
-    selfref: string
-}
